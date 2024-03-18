3ce91bc867829f5436c6ad1e340d135fd96505c4
teo kurtagic <teo@sl-king.com>
2024-03-18 23:41:50 +0100

    fixed vaje04

--- vaje/vaje-04/01-permutacija/permutacija.c
+++ vaje/vaje-04/01-permutacija/permutacija.c
@@ -1,19 +1,25 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-int tabela[1000000];
+bool jeClenPermutacije(int *array, int len, int n);
 
 int main()
 {
     int n;
     scanf("%d", &n);
+    int tabela[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        tabela[i] = -1;
+    }
 
     for (int i = 0; i < n; i++)
     {
         int a;
         scanf("%d", &a);
 
-        if (a < 0 || a >= n || tabela[a] != 0)
+        if (!jeClenPermutacije(tabela, n, a))
         {
             printf("NE\n");
 
@@ -24,5 +30,11 @@ int main()
     }
 
     printf("DA\n");
+
     return 0;
+}
+
+bool jeClenPermutacije(int *array, int len, int n)
+{
+    return n >= 0 && n < len && array[n] == -1;
 }
--- vaje/vaje-04/02-najvecje-podzaporedje/najvecje_podzaporedje.c
+++ vaje/vaje-04/02-najvecje-podzaporedje/najvecje_podzaporedje.c
@@ -1,33 +1,42 @@
 #include <limits.h>
 #include <stdio.h>
-int tabela[1000000];
+
+int maxVsota(int *array, int len);
 
 int main()
 {
     int n;
     scanf("%d", &n);
 
+    int tabela[n];
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &tabela[i]);
     }
 
+    printf("%d\n", maxVsota(tabela, n));
+
+    return 0;
+}
+
+int maxVsota(int *array, int len)
+{
     int max = INT_MIN;
     int sum = 0;
-    for (int i = 0; i < n; i++)
+
+    for (int i = 0; i < len; i++)
     {
-        sum += tabela[i];
+        sum += array[i];
         if (sum > max)
         {
             max = sum;
         }
-        if (sum <= 0)
+
+        if (sum < 0)
         {
             sum = 0;
         }
     }
 
-    printf("%d\n", max);
-
-    return 0;
+    return max;
 }

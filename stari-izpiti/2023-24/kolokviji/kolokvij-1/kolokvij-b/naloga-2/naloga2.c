12641c82f4837c358e3f256038893eeaf054ec92
teo kurtagic <teo@sl-king.com>
2024-04-13 17:45:02 +0200

    added shit fuck

--- stari-izpiti/2023-24/kolokviji/kolokvij-1/kolokvij-b/naloga-2/naloga2.c
+++ stari-izpiti/2023-24/kolokviji/kolokvij-1/kolokvij-b/naloga-2/naloga2.c
@@ -1,48 +1,16 @@
-
-/*
- * Prevajanje in zagon testnega programa testXY.c:
- *
- * gcc -D=test testXY.c naloga2.c
- * ./a.out
- *
- * Zagon testne skripte ("sele potem, ko ste prepri"cani, da program deluje!):
- *
- * export name=naloga2
- * make test
- *
- * Javni testni primeri (po te"zavnosti):
- * -- 02, 03: dol"zina vsakega vhodnega niza je enaka ciljnaDolzina;
- * -- 04, 05, 06, 07: dol"zina vsakega vhodnega niza je enaka 1;
- * -- 01, 08, 09, 10: splo"sni primeri.
- *
- * Javni testni primeri (po na"cinu priprave):
- * -- 01: primer iz besedila;
- * -- 01, 02, 04: ro"cno ustvarjeni;
- * -- ostali: samodejno generirani.
- */
-
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-// po potrebi dopolnite ...
-
-//=============================================================================
-
-char** naSredino(char** nizi, int ciljnaDolzina) {
-    // popravite / dopolnite ...
+char **naSredino(char **nizi, int ciljnaDolzina)
+{
     return NULL;
 }
-
-//=============================================================================
-
 #ifndef test
 
-int main() {
-    // "Ce "zelite funkcijo naSredino testirati brez testnih primerov,
-    // dopolnite to funkcijo in prevedite datoteko na obi"cajen na"cin
-    // (gcc naloga2.c).
+int main()
+{
     return 0;
 }
 

a8ba5e8096514582037660327890e9e571f9b994
teo kurtagic <teo@sl-king.com>
2024-04-19 14:09:30 +0200

    added kolokvij-b solutions

--- stari-izpiti/2023-24/kolokviji/kolokviji-1/kolokvij-b/naloga-1/naloga1.c
+++ stari-izpiti/2023-24/kolokviji/kolokviji-1/kolokvij-b/naloga-1/naloga1.c
@@ -1,9 +1,35 @@
-#include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
+void printRange(int n, int len, int increment);
 
 int main()
 {
+    int n;
+    scanf("%d", &n);
+
+    int len;
+    scanf("%d", &len);
+    len++;
+
+    int number = 0;
+    int increment = 1;
+    for (int i = 0; i < n; i++)
+    {
+        printRange(number, len, increment);
+
+        number += (len - 3) * increment;
+        scanf("%d", &len);
+        increment *= -1;
+    }
+
     return 0;
+}
+
+void printRange(int n, int len, int increment)
+{
+    for (int i = 0; i < len - 1; i++)
+    {
+        printf("%d\n", n);
+        n += increment;
+    }
 }
--- stari-izpiti/2023-24/kolokviji/kolokviji-1/kolokvij-b/skriti-testi/naloga1/naloga1.c
+++ stari-izpiti/2023-24/kolokviji/kolokviji-1/kolokvij-b/skriti-testi/naloga1/naloga1.c
@@ -1,66 +1,35 @@
-
-/*
- * Zagon testne skripte ("sele potem, ko ste prepri"cani, da program deluje!):
- *
- * export name=naloga1
- * make test
- */
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
 
-// po potrebi dopolnite ...
+void printRange(int n, int len, int increment);
 
-int main() {
+int main()
+{
     int n;
     scanf("%d", &n);
 
-    int *zaporedje = (int *)malloc(n * sizeof(int));
-    
-    for(int i = 0; i < n; i++)
+    int len;
+    scanf("%d", &len);
+    len++;
+
+    int number = 0;
+    int increment = 1;
+    for (int i = 0; i < n; i++)
     {
-        scanf("%d", &zaporedje[i]);
+        printRange(number, len, increment);
+
+        number += (len - 3) * increment;
+        scanf("%d", &len);
+        increment *= -1;
     }
-
-    int status = 0;
-    int counter = 1;
-    for(int i = 1; i < n; i++)
-    {
-
-        if(status == 0)
-        {
-            if(zaporedje[i] > zaporedje[i - 1])
-            {
-                status = 1;
-            }
-            else
-            {
-                status = -1;
-            }
-
-            counter++;
-            continue;
-        }
-
-        if(status == 1 && zaporedje[i] > zaporedje[i - 1])
-        {
-            counter++;
-            continue;
-        }
-        
-        if(status == -1 && zaporedje[i] < zaporedje[i - 1])
-        {
-            counter++;
-            continue;
-        }
-
-        printf("%d\n", counter);
-        counter = 2;
-        status = (status == 1) ? -1 : 1;
-    }
-    printf("%d\n", counter);
 
     return 0;
+}
+
+void printRange(int n, int len, int increment)
+{
+    for (int i = 0; i < len - 1; i++)
+    {
+        printf("%d\n", n);
+        n += increment;
+    }
 }

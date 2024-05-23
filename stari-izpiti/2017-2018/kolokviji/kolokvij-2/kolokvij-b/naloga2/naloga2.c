9560767c66b67905874a45d3f424bc4acb7aebdf
teo kurtagic <teo@sl-king.com>
2024-05-23 19:23:30 +0200

    added 2017/k2/kb/naloga2

--- stari-izpiti/2017-2018/kolokviji/kolokvij-2/kolokvij-b/naloga2/naloga2.c
+++ stari-izpiti/2017-2018/kolokviji/kolokvij-2/kolokvij-b/naloga2/naloga2.c
@@ -1,9 +1,74 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
+#define UNSET -1
+#define NOT_SET 0
+#define SET 1
+
+int variations(int n, int k, int *isPlaced, int index);
 
 int main()
 {
+    int k;
+    int n;
+
+    scanf("%d %d", &k, &n);
+
+    int *placements = (int *)malloc(k * sizeof(int));
+    for (int i = 0; i < k; i++)
+    {
+        placements[i] = UNSET;
+    }
+
+    int count = 0;
+    placements[0] = SET;
+    count += variations(k, n - 1, placements, 1);
+
+    placements[0] = NOT_SET;
+    count += variations(k, n, placements, 1);
+
+    printf("%d\n", count);
+
+    free(placements);
+
     return 0;
+}
+
+int variations(int k, int n, int *placements, int index)
+{
+    if (n < 0)
+    {
+        return 0;
+    }
+
+    if (index == k && n > 0)
+    {
+        return 0;
+    }
+
+    if (index == k && n == 0)
+    {
+        return 1;
+    }
+
+    int count = 0;
+    int previous = placements[index - 1];
+
+    if (previous == SET)
+    {
+        placements[index] = NOT_SET;
+        count += variations(k, n, placements, index + 1);
+    }
+
+    if (previous == NOT_SET)
+    {
+        placements[index] = SET;
+        count += variations(k, n - 1, placements, index + 1);
+
+        placements[index] = NOT_SET;
+        count += variations(k, n, placements, index + 1);
+    }
+
+    return count;
 }

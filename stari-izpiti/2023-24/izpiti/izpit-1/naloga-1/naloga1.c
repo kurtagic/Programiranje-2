025bc032e5f3042c3095a8baa11c4fca8fe1b900
teo kurtagic <teo@sl-king.com>
2024-06-11 18:09:35 +0200

    ....no comment..spet zajebu p2....

---
+++ stari-izpiti/2023-24/izpiti/izpit-1/naloga-1/naloga1.c
@@ -0,0 +1,23 @@
+
+/*
+ * Zagon testne skripte ("sele potem, ko ste prepri"cani, da program deluje!):
+ *
+ * export name=naloga1
+ * make test
+ *
+ * Javni testni primeri:
+ * 02..06: v vsakem podprimeru je "stevilo polj enako n^2
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+// po potrebi dopolnite ...
+
+int main(int argc, char** argv) {
+    // dopolnite ...
+
+    return 0;
+}
---
+++ stari-izpiti/2023-24/izpiti/izpit-1/naloga-2/naloga2.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+
+#include "naloga2.h"
+
+void obdelaj(Vozlisce *zacetek, int k)
+{
+    Vozlisce *current = zacetek;
+    Vozlisce *previous = NULL;
+
+    int *buffer = (int *)malloc(k * sizeof(int));
+
+    int index = 0;
+    int sum = 0;
+
+    while (current != NULL)
+    {
+        int podatek = current->podatek;
+
+        if (index < k)
+        {
+            sum += podatek;
+            buffer[index++] = podatek;
+
+            previous = current;
+            current = current->naslednje;
+
+            continue;
+        }
+
+        if (podatek == sum)
+        {
+            current = current->naslednje;
+            previous->naslednje = current;
+
+            continue;
+        }
+
+        sum = sum - buffer[index % k] + podatek;
+        buffer[index++ % k] = podatek;
+
+        previous = current;
+        current = current->naslednje;
+    }
+
+    free(buffer);
+}
+
+#ifndef test
+int main()
+{
+    return 0;
+}
+#endif

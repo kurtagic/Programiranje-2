d395de0c3359b993f8463ae26e80d00f79e6b0f6
teo kurtagic <teo@sl-king.com>
2024-08-22 08:40:02 +0200

    fjdkafsj

--- stari-izpiti/2020-21/izpit-2/naloga-2/script.py
+++ stari-izpiti/2020-21/izpit-2/naloga-2/script.py
@@ -1,10 +1,9 @@
 import struct
 
-binary_numbers = [0b0000010, 0b00001111, 0b10001001, 0b10000000, 0b10000000]  # This will write 4 bytes
+binary_numbers = [0b0000010, 0b00001111, 0b10001001, 0b10000000, 0b10000000]
 
 binary_data = struct.pack('5B', *binary_numbers)
 
 with open('input.bin', 'wb') as binary_file:
     binary_file.write(binary_data)
 
-print("Binary numbers written successfully.")

c95076e762586f23cd1e8af5c372dcc7f62082f6
teo kurtagic <teo@sl-king.com>
2024-08-22 22:56:39 +0200

    fjafjds

--- dn/dn-11/naloga-1/naloga1.c
+++ dn/dn-11/naloga-1/naloga1.c
@@ -1,21 +1,72 @@
-
-#include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
 
 #include "naloga1.h"
 
-Vozlisce* zdesetkaj(Vozlisce* zacetek, int k) {
-    // popravite / dopolnite ...
-    return NULL;
+void deleteNodesBetween(Vozlisce *start, Vozlisce *stop);
+
+Vozlisce *zdesetkaj(Vozlisce *zacetek, int k)
+{
+    Vozlisce *current = zacetek;
+    Vozlisce *previousKth = NULL;
+
+    for (int i = 1; current != NULL; i++)
+    {
+        if (i % k != 0)
+        {
+            current = current->naslednje;
+
+            continue;
+        }
+
+        // set head as first k-th node
+        if (previousKth == NULL)
+        {
+            deleteNodesBetween(zacetek, current);
+
+            zacetek = current;
+            previousKth = current;
+
+            current = current->naslednje;
+
+            continue;
+        }
+
+        // head is set, continue with current k-th node
+
+        deleteNodesBetween(previousKth->naslednje, current);
+
+        previousKth->naslednje = current;
+        previousKth = previousKth->naslednje;
+
+        current = current->naslednje;
+    }
+
+    // remove nodes after last k-th element
+    if (previousKth != NULL)
+    {
+        deleteNodesBetween(previousKth->naslednje, NULL);
+    }
+
+    previousKth->naslednje = NULL;
+
+    return zacetek;
+}
+
+// frees node in interval including start, excluding stop ( Interval = [start, stop) )
+void deleteNodesBetween(Vozlisce *start, Vozlisce *stop)
+{
+    Vozlisce *current = start;
+    while (current != NULL && current != stop)
+    {
+        Vozlisce *temp = current;
+        current = current->naslednje;
+        free(temp);
+    }
 }
 
 #ifndef test
-
-int main() {
-    // koda za ro"cno testiranje (po "zelji)
+int main()
+{
     return 0;
 }
-
 #endif
---
+++ dn/dn-12/naloga-1/grda.c
@@ -0,0 +1,19 @@
+               #include <stdio.h>
+     #include <stdlib.h>
+  
+            int main() {
+int n;
+        scanf("%d", &n);
+
+        int stKorakov = 0;
+while (n > 1) {
+                if (n % 2 == 0) {
+    n /= 2;
+           } else {
+        n = 3 * n + 1;
+            }
+          stKorakov++;
+             }
+        printf("%d\n", stKorakov);
+               return 0;
+         }

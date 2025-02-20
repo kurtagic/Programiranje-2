3feaf2f316786c1821227241da0354df7f9285ec
kurtagic <teo@sl-king.com>
2025-02-20 10:54:55 +0100

    asdflkj

--- 2024-25/stari-kolokviji-in-izpiti/2023-24/kolokviji/kolokvij-1/kolokvij-a/naloga-3/naloga3.c
+++ 2024-25/stari-kolokviji-in-izpiti/2023-24/kolokviji/kolokvij-1/kolokvij-a/naloga-3/naloga3.c
@@ -1,20 +1,63 @@
-
-/*
- * Zagon testne skripte ("sele potem, ko ste prepri"cani, da program deluje!):
- *
- * export name=naloga3
- * make test
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
 
-// po potrebi dopolnite ...
+void print_array(int* array, int n);
 
-int main() {
-    // dopolnite ...
+int main()
+{
+	int v;
+	int s;
 
-    return 0;
+	scanf("%d %d", &v, &s);
+
+	int* zaporedje_A = (int*)malloc(v * s * sizeof(int));
+	int* zaporedje_B = (int*)malloc((v + 1) * sizeof(int));
+	int* zaporedje_C = (int*)malloc(v * s * sizeof(int));
+
+	int index_A_C = 0;
+	zaporedje_B[0] = 0;
+
+	for (int i = 0; i < v; i++)
+	{
+		zaporedje_B[i + 1] = zaporedje_B[i];
+
+		for (int j = 0; j < s; j++)
+		{
+			int element;
+			scanf("%d", &element);
+
+			if (element == 0)
+			{
+				continue;
+			}
+
+			zaporedje_A[index_A_C] = element;
+			zaporedje_C[index_A_C] = j;
+			index_A_C++;
+			zaporedje_B[i + 1]++;
+		}
+	}
+
+	print_array(zaporedje_A, index_A_C);
+	print_array(zaporedje_B, v + 1);
+	print_array(zaporedje_C, index_A_C);
+
+	free(zaporedje_A);
+	free(zaporedje_B);
+	free(zaporedje_C);
+
+	return 0;
+}
+
+void print_array(int* array, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d", array[i]);
+		printf(i + 1 < n ? "_" : "");
+	}
+
+	printf("\n");
 }
